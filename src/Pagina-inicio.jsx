dee07f3a8b11e1de787387cd60ae972a28d9a8ab
Paul <alphaceves@gmail.com>
2021-02-14 20:29:06 -0600

    Creacion de la pagina principal de skyder

---
+++ src/Pagina-inicio.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Inicio = () => (
+    <div className='inicio'>
+        <div className='video-contenedor'>
+            <div className='video-video'>
+                <video src="" autoplay muted/>
+            </div>
+            <div className='video-texto'>
+            <h1>que tranza banda gg</h1>
+            <span>texto mamalon xd</span>
+            </div>
+        </div>
+        <div className='empresas-contenedor'>
+            <div className='empresas-texto'>
+                <h1>Conoce alguno de nuestros clientes</h1>
+            </div>
+            <div className='empresas-logos'>
+
+            </div>
+        </div>
+    </div> 
+)
+
+export default Inicio;
